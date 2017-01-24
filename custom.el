75afdc44873a809bb2388cf8aa6e0b7389c8acce
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-01-24 21:22:20 +0800

    * golden-ratio-mode t

--- custom.el
+++ custom.el
@@ -4,7 +4,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(global-hungry-delete-mode t)
- '(golden-ratio-mode t))
+ '(golden-ratio-mode t)
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
