c71d12d8b7d5e4d83d29638e23cf32958b19cbd8
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-03-06 14:37:33 +0800

    * format

--- init.el
+++ init.el
@@ -36,12 +36,11 @@ values."
      ;; Uncomment some layer names and press <SPC f e R> (Vim style) or
      ;; <M-m f e R> (Emacs style) to install them.
      ;; ----------------------------------------------------------------
-     ;; helm
      ivy
      (auto-completion :variables
+                      ;; auto-completion-return-key-behavior nil
                       auto-completion-enable-sort-by-usage t
-                      auto-completion-enable-snippets-in-popup t
-      )
+                      auto-completion-enable-snippets-in-popup t)
      better-defaults
      ;; (better-defaults :variables
      ;;                  better-defaults-move-to-beginning-of-code-first t
