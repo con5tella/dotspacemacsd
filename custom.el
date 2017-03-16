513e390c4f69e5c338b46bb7f366e6034266134d
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-03-16 10:16:37 +0800

    - custom set-face-attribute

--- custom.el
+++ custom.el
@@ -11,10 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-
- ;; set face-attribute font, disabling in default theme
- (set-face-attribute 'font-lock-function-name-face nil :weight 'bold)
- (set-face-attribute 'font-lock-type-face nil :weight 'semi-bold :slant 'italic)
- (set-face-attribute 'font-lock-comment-face nil :slant 'oblique)
-
  )
