03480d5d41c76af12f065a0de5a02215296073bf
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-04-14 18:29:48 +0800

    * theme anti-zenburn

--- init.el
+++ init.el
@@ -165,11 +165,11 @@ values."
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
    dotspacemacs-themes '(
+                         anti-zenburn
                          ;; spacemacs-dark
                          ;; spacemacs-light
                          ;; sanityinc-solarized-light
                          ;; sanityinc-tomorrow-eighties
-                         zenburn
                          ;; leuven
                          )
    ;; If non-nil the cursor color matches the state color in GUI Emacs.
