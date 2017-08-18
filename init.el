42a282aae078c9d3f9ab8f15d39fc6a7dafb216d
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-08-18 21:25:26 +0800

    + bookmarks

--- init.el
+++ init.el
@@ -185,9 +185,9 @@ It should only modify the values of Spacemacs settings."
    ;; `recents' `bookmarks' `projects' `agenda' `todos'.
    ;; List sizes may be nil, in which case
    ;; `spacemacs-buffer-startup-lists-length' takes effect.
-   dotspacemacs-startup-lists '((recents . 10)
-                                ;; (projects . 7)
-                                )
+   dotspacemacs-startup-lists '((recents . 5)
+                                (bookmarks . 7)
+                                (projects . 7))
    ;; True if the home buffer should respond to resize events. (default t)
    dotspacemacs-startup-buffer-responsive t
    ;; Default major mode of the scratch buffer (default `text-mode')
