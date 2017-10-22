72906a6fba7c335e6263f5563105aff6380f550f
Yuanchen Xie <yuanchen.gm@gmail.com>
2017-10-22 11:31:05 +0800

    * merge & bk

--- init.el
+++ init.el
@@ -109,7 +109,7 @@ This function should only modify configuration layer settings."
                                     spaceline symon
                                     tetris
                                     volatile-highlights
-                                    window-purpose
+                                    ;; window-purpose
                                     )
    ;; Defines the behaviour of Spacemacs when installing packages.
    ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
