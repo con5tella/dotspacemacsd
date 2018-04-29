a163e98ec4d59eb100886407e9ef842691c68574
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-04-29 13:41:25 +0800

    * initial-scratch-message

--- init.el
+++ init.el
@@ -181,7 +181,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Initial message in the scratch buffer, such as "Welcome to Spacemacs!"
    ;; (default nil)
-   dotspacemacs-initial-scratch-message nil
+   dotspacemacs-initial-scratch-message "Hello, Yuanchen!\n"
 
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
