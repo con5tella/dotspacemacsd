7669365a1aaf8b87e8d16c5f8b108d112ef8ccc1
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-06-19 11:04:51 +0800

    + python-backend 'anaconda

--- init.el
+++ init.el
@@ -57,7 +57,8 @@ This function should only modify configuration layer settings."
      (org :variables
           org-enable-reveal-js-support t)
      perl5
-     python
+     (python :variables
+             python-backend 'anaconda)
      (shell :variables
             shell-default-height 30
             shell-default-position 'bottom)
