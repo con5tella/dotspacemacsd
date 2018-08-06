b66fa03ce5c5804fa3075c1597f91e1583316941
Yuanchen Xie <yuanchen.gm@gmail.com>
2018-08-06 20:29:26 +0800

    - ivy layer

--- init.el
+++ init.el
@@ -40,7 +40,7 @@ This function should only modify configuration layer settings."
      ;; `M-m f e R' (Emacs style) to install them.
      ;; ----------------------------------------------------------------
      helm
-     ivy
+     ;; ivy
      auto-completion
      ;; better-defaults
      ;; bibtex
